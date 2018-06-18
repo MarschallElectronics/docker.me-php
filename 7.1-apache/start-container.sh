2a26938814d6d4e40932a93c68519df12ad938a9
Night <ffxtobias@gmail.com>
2018-06-18 10:14:47 +0200

    Docker me-base_php     - PHP 5.6 Version     - PHP 7.1 Version         - Beide versionen RemoteProxy config default gesetzt
    Tobias Bergkofer

--- 7.1-apache/start-container.sh
+++ 7.1-apache/start-container.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ##################################################################################################################
+# Globale Host IP
+export DOCKER_HOST_IP="$(/sbin/ip route|awk '/default/ { print $3 }')"
+
+##################################################################################################################
 # Unable to get Full Qualified Servername Workaround
 echo "ServerName "$SERVER_NAME >> /etc/apache2/apache2.conf
 
@@ -11,6 +15,10 @@ sed -i 's/'hostname=.*'/'hostname=$SERVER_NAME'/g' /etc/ssmtp/ssmtp.conf
 
 ##################################################################################################################
 # RemoteIp Config
+if [ -z "$REMOTE_IP_PROXY" ]
+then
+       export REMOTE_IP_PROXY=$DOCKER_HOST_IP
+fi
 sed -i 's/'RemoteIPTrustedProxy.*'/'RemoteIPTrustedProxy\ $REMOTE_IP_PROXY'/g' /etc/apache2/conf-available/remoteip.conf
 
 ##################################################################################################################
