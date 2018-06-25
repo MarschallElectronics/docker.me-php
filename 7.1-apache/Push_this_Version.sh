fcd48dd23ebbf4b82417cbeb11d87dc5c6f35837
Night <ffxtobias@gmail.com>
2018-06-25 14:28:06 +0200

    Docker me-base_php     - PHP 5.6 Version     - PHP 7.1 Version     - PHP 7.2 Version
    Tobias Bergkofer

--- 7.1-apache/Push_this_Version.sh
+++ 7.1-apache/Push_this_Version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build -t marschallelectronics/me_base-php:7.1-apache .
-#docker push marschallelectronics/me_base-php:7.1-apache
+docker push marschallelectronics/me_base-php:7.1-apache
 
 read -p "Press Enter to Exit" var
---
+++ 7.2-apache/Push_this_Version.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -t marschallelectronics/me_base-php:7.2-apache .
+#docker push marschallelectronics/me_base-php:7.2-apache
+
+read -p "Press Enter to Exit" var
