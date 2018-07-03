a0a978eb86eda85c89c2bcd48d88ce156c26ad13
Bene <br@garmisch.net>
2018-07-03 10:33:32 +0200

    dockerfiles, global-tests und so

---
+++ bene/BuildAndPush.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOCKERFILE=$1
+TAG=''
+
+if [ -e ${DOCKERFILE} ]
+then
+	TAG=$(echo ${DOCKERFILE} | grep "^Dockerfile-" | cut -c 12-)
+fi
+
+if [ "X" != "X${TAG}" ]
+then
+	docker build -t marschallelectronics/me_base-php:${TAG} -f ${DOCKERFILE} .
+	docker push marschallelectronics/me_base-php:${TAG}
+fi
