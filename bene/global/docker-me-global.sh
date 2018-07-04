2a93dc6e17a4f8f803330c91dea60ebee2882e20
Bene <br@garmisch.net>
2018-07-04 13:51:27 +0200

    tests soap

--- bene/global/docker-me-global.sh
+++ bene/global/docker-me-global.sh
@@ -9,8 +9,8 @@ apt-get update \
     && apt-get upgrade -y
 
 # diverse Apps
-apt-get install -y --no-install-recommends locales apt-transport-https nano git net-tools iproute2 mailutils gnupg \
-	&& apt-get install -y libbz2-dev libfreetype6-dev libjpeg62-turbo-dev libpng-dev libxpm-dev libvpx-dev libmcrypt-dev libmemcached-dev \
+apt-get install -y locales apt-transport-https nano git net-tools iproute2 mailutils gnupg libxml2-dev \
+	libbz2-dev libfreetype6-dev libjpeg62-turbo-dev libpng-dev libxpm-dev libvpx-dev libmcrypt-dev libmemcached-dev \
 
 # postfix
 echo "postfix postfix/main_mailer_type string Internet site" > /tmp/preseed.txt \
@@ -25,7 +25,8 @@ echo "de_DE.UTF-8 UTF-8" > /etc/locale.gen \
 
 # PHP-Extensions
 docker-php-ext-configure gd --with-freetype-dir=/usr/lib/x86_64-linux-gnu/ --with-jpeg-dir=/usr/lib/x86_64-linux-gnu/ --with-xpm-dir=/usr/lib/x86_64-linux-gnu/ \
-	&& docker-php-ext-install gd pdo pdo_mysql mysqli \
+	&& docker-php-ext-install gd \
+	&& docker-php-ext-install pdo pdo_mysql mysqli soap \
 	&& pear install DB \
 	&& pear install DB_Dataobject \
 
