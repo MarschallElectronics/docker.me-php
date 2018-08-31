b237a1b561ea5d0e931647ba96d96fdf7790c082
Bene <br@garmisch.net>
2018-08-31 14:16:55 +0200

    wget

--- global/docker-me-global.sh
+++ global/docker-me-global.sh
@@ -14,7 +14,7 @@ echo "# diverse Apps"
 apt-get install -y locales apt-transport-https nano git net-tools iproute2 mailutils gnupg libxml2-dev mysql-client \
 	libbz2-dev libfreetype6-dev libjpeg62-turbo-dev libpng-dev libxpm-dev libvpx-dev libmcrypt-dev libmemcached-dev \
 	libsqlite3-dev libssl-dev libz-dev libz-dev zlib1g-dev libsqlite3-dev zip libxml2-dev \
-	libcurl3-dev libedit-dev libpspell-dev libldap2-dev unixodbc-dev libpq-dev
+	libcurl3-dev libedit-dev libpspell-dev libldap2-dev unixodbc-dev libpq-dev wget
 
 echo "# Bugfix: libldap (https://bugs.php.net/bug.php?id=49876)"
 ln -fs /usr/lib/x86_64-linux-gnu/libldap.so /usr/lib/
