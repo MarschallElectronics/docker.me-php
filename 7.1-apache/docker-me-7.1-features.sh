c103ac3a4e89f351f2769022eeaa0735a46f2448
Markus <ml@garmisch.net>
2019-02-25 10:19:04 +0100

    docker-me-7.1 imagick Rückgängig

--- 7.1-apache/docker-me-7.1-features.sh
+++ 7.1-apache/docker-me-7.1-features.sh
@@ -6,9 +6,7 @@ echo "###############################################"
 echo "ME: install-7.1-features"
 echo "###############################################"
 
-# PHP-Ext: APCU, iconv, mbstring, Mcrypt, imagick (Deprecated in 7.2)
+# PHP-Ext: APCU, iconv, mbstring, Mcrypt (Deprecated in 7.2)
 pecl install apcu \
 	&& docker-php-ext-enable apcu \
-	&& docker-php-ext-install iconv mbstring mcrypt \
-	&& pecl install imagick \
-    && docker-php-ext-enable imagick
+	&& docker-php-ext-install iconv mbstring mcrypt
