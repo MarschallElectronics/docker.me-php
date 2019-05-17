cb6d009bb62521794713f7361e7f763e4b9febd0
Bene <br@garmisch.net>
2019-05-17 16:47:39 +0200

    php7.3

---
+++ 7.3-apache/docker-me-7.3-features.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo "###############################################"
+echo "ME: install-7.3-features"
+echo "###############################################"
+
+# PHP-Ext: APCU, iconv, mbstring
+pecl install apcu \
+	&& docker-php-ext-enable apcu \
+	&& docker-php-ext-install iconv mbstring
