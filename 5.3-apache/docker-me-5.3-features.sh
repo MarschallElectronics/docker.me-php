9d76a00b3036a1e1042a7a6929928a4f57ded71b
Bene <br@garmisch.net>
2020-06-15 15:56:40 +0200

    5.3-shit

--- 5.3-apache/docker-me-5.3-features.sh
+++ 5.3-apache/docker-me-5.3-features.sh
@@ -8,20 +8,11 @@ echo "###############################################"
 
 echo "# Update & Upgrade"
 echo "-------------------------------"
-apt-get update
+apt-get -y update && apt-get -y upgrade
 
 echo "# + install diverse Apps"
 echo "-------------------------------"
-apt-get install -y locales apt-transport-https nano git net-tools mailutils gnupg wget gcc make
-
-echo "# + install postfix"
-echo "-------------------------------"
-echo "postfix postfix/main_mailer_type string Internet site" > /tmp/preseed.txt \
-  && echo "postfix postfix/mailname string docker_web1.garmisch.net" >> /tmp/preseed.txt \
-  && debconf-set-selections /tmp/preseed.txt \
-  && DEBIAN_FRONTEND=noninteractive \
-  && apt-get install -y postfix \
-  && rm /tmp/preseed.txt
+apt-get install -y locales apt-transport-https nano git net-tools mailutils gnupg wget gcc make ssmtp
 
 echo "# + install Language DE"
 echo "-------------------------------"
@@ -35,24 +26,40 @@ a2enmod rewrite \
   && a2enmod expires \
   && a2enmod headers
 
-echo "# + config Postfix"
-echo "-------------------------------"
+# a2enmod remoteip # gibts nicht für apache 2.2 (php 5.3 gibts nur für apache 2.2)
+
+
+#
+# docker image funktioniert nur mit ssmtp weil man werte in php.ini nicht ändern kann und dort als sendmailer ssmtp angegeben ist
+#
+
+#echo "# + install postfix"#
+#echo "-------------------------------"
+#echo "postfix postfix/main_mailer_type string Internet site" > /tmp/preseed.txt \
+#  && echo "postfix postfix/mailname string docker_web1.garmisch.net" >> /tmp/preseed.txt \
+#  && debconf-set-selections /tmp/preseed.txt \
+#  && DEBIAN_FRONTEND=noninteractive \
+#  && apt-get install -y postfix \
+#  && rm /tmp/preseed.txt
+
+#echo "# + config Postfix"
+#echo "-------------------------------"
 
 # nur IPv4
-sed -i "s/inet_protocols.*=.*/inet_protocols = ipv4/g" /etc/postfix/main.cf
+#sed -i "s/inet_protocols.*=.*/inet_protocols = ipv4/g" /etc/postfix/main.cf
 
 # SASL vorbereiten
-echo "smtp_sasl_auth_enable = no" >> /etc/postfix/main.cf
-echo "smtp_sasl_security_options = noanonymous" >> /etc/postfix/main.cf
-echo "smtp_tls_security_level = none" >> /etc/postfix/main.cf
-echo "smtp_sasl_password_maps = hash:/etc/postfix/sasl_password" >> /etc/postfix/main.cf
-echo "" > /etc/postfix/sasl_password
-postmap /etc/postfix/sasl_password
+#echo "smtp_sasl_auth_enable = no" >> /etc/postfix/main.cf
+#echo "smtp_sasl_security_options = noanonymous" >> /etc/postfix/main.cf
+#echo "smtp_tls_security_level = none" >> /etc/postfix/main.cf
+#echo "smtp_sasl_password_maps = hash:/etc/postfix/sasl_password" >> /etc/postfix/main.cf
+#echo "" > /etc/postfix/sasl_password
+#postmap /etc/postfix/sasl_password
 
 # Sender
-echo "#sender_canonical_maps = regexp:/etc/postfix/sender_canonical" >> /etc/postfix/main.cf
-echo "" > /etc/postfix/sender_canonical
+#echo "#sender_canonical_maps = regexp:/etc/postfix/sender_canonical" >> /etc/postfix/main.cf
+#echo "" > /etc/postfix/sender_canonical
 
 # Header
-echo "#smtp_header_checks = regexp:/etc/postfix/header_check" >> /etc/postfix/main.cf
-echo "" > /etc/postfix/header_check
+#echo "#smtp_header_checks = regexp:/etc/postfix/header_check" >> /etc/postfix/main.cf
+#echo "" > /etc/postfix/header_check
