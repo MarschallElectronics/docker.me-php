d94de791ce0e59da68131be4842a6804b5db2b7b
Bene <bene@zipzop.de>
2020-11-24 22:06:05 +0100

    latest

--- BuildAndPush.sh
+++ BuildAndPush.sh
@@ -3,15 +3,24 @@
 DOCKERFILE=$1
 TAG=''
 
-if [ -e ${DOCKERFILE} ]
+if [ -e "${DOCKERFILE}" ]
 then
-	TAG=$(echo ${DOCKERFILE} | grep "^Dockerfile-" | cut -c 12-)
+	TAG=$(echo "${DOCKERFILE}" | grep "^Dockerfile-" | cut -c 12-)
 fi
 
 if [ "X" != "X${TAG}" ]
 then
-	docker build --no-cache -t marschallelectronics/me-php:${TAG} -f ${DOCKERFILE} . || exit 1
-	docker push marschallelectronics/me-php:${TAG} || exit 1
+	docker build --no-cache -t "marschallelectronics/me-php:${TAG}" -f "${DOCKERFILE}" . || exit 1
+	docker push "marschallelectronics/me-php:${TAG}" || exit 1
+else
+	echo "No TAG available!"
+fi
+
+# latest
+if [ "${TAG}" == "7.4-apache" ]
+then
+	docker build --no-cache -t marschallelectronics/me-php:latest -f "${DOCKERFILE}" . || exit 1
+	docker push marschallelectronics/me-php:latest || exit 1
 else
 	echo "No TAG available!"
 fi
