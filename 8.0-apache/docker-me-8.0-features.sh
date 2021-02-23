4b6811a1460553c67eb48d7c67af545a5440d7f3
Bene <bene@zipzop.de>
2021-02-23 09:17:40 +0100

    php8.0

--- 8.0-apache/docker-me-8.0-features.sh
+++ 8.0-apache/docker-me-8.0-features.sh
@@ -42,7 +42,8 @@ echo "-------------------------------"
 #  && rm -r /var/lib/apt/lists/*
 
 # workaround mit warnings
-mkdir -p /usr/src/php/ext/imagick \
+apt-get -y install libmagickwand-dev --no-install-recommends \
+  && mkdir -p /usr/src/php/ext/imagick \
   && curl -fsSL https://github.com/Imagick/imagick/archive/06116aa24b76edaf6b1693198f79e6c295eda8a9.tar.gz | tar xvz -C "/usr/src/php/ext/imagick" --strip 1 \
   && docker-php-ext-install imagick
 
