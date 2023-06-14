6640daf15f3b20d590236a0420e8914b6109aa06
Bene <br@garmisch.net>
2023-06-14 15:02:05 +0200

    div

--- BuildAndPushAll.sh
+++ BuildAndPushAll.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-echo -e "---------------------\n5.6-apache:\n---------------------\n\n" \
-&& bash BuildAndPush.sh Dockerfile-5.6-apache \
 && echo -e "---------------------\n7.0-apache:\n---------------------\n\n" \
 && bash BuildAndPush.sh Dockerfile-7.0-apache \
 && echo -e "---------------------\n7.1-apache:\n---------------------\n\n" \
 && bash BuildAndPush.sh Dockerfile-7.1-apache \
-&& echo -e "---------------------\n7.1-apache-jessie:\n---------------------\n\n" \
-&& bash BuildAndPush.sh Dockerfile-7.1-apache-jessie \
-&& echo -e "---------------------\n7.1-apache-sqlsrv:\n---------------------\n\n" \
-&& bash BuildAndPush.sh Dockerfile-7.1-apache-sqlsrv \
 && echo -e "---------------------\n7.2-apache:\n---------------------\n\n" \
 && bash BuildAndPush.sh Dockerfile-7.2-apache \
 && echo -e "---------------------\n7.3-apache:\n---------------------\n\n" \
@@ -20,7 +14,5 @@ echo -e "---------------------\n5.6-apache:\n---------------------\n\n" \
 && bash BuildAndPush.sh Dockerfile-8.0-apache \
 && echo -e "---------------------\n8.1-apache:\n---------------------\n\n" \
 && bash BuildAndPush.sh Dockerfile-8.1-apache \
-&& echo -e "---------------------\n8.1-apache-bullseye:\n---------------------\n\n" \
-&& bash BuildAndPush.sh Dockerfile-8.1-apache-bullseye \
 && echo -e "---------------------\n8.2-apache:\n---------------------\n\n" \
 && bash BuildAndPush.sh Dockerfile-8.2-apache
