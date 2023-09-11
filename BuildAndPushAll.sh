af956e2dde7880da5c27c9c7b59a730985e90560
Bene <br@garmisch.net>
2023-09-11 14:59:06 +0200

    div

--- BuildAndPushAll.sh
+++ BuildAndPushAll.sh
@@ -6,5 +6,7 @@ echo -e "---------------------\n7.4-apache:\n---------------------\n\n" \
 && bash BuildAndPush.sh Dockerfile-8.0-apache \
 && echo -e "---------------------\n8.1-apache:\n---------------------\n\n" \
 && bash BuildAndPush.sh Dockerfile-8.1-apache \
+&& echo -e "---------------------\n8.1-apache-bullseye:\n---------------------\n\n" \
+&& bash BuildAndPush.sh Dockerfile-8.1-apache-bullseye \
 && echo -e "---------------------\n8.2-apache:\n---------------------\n\n" \
 && bash BuildAndPush.sh Dockerfile-8.2-apache
