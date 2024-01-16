cadee1996e7dadd65ca43b29e03ce3859683baf1
Bene <br@garmisch.net>
2024-01-16 15:07:49 +0100

    8.3

--- BuildAndPushAll.sh
+++ BuildAndPushAll.sh
@@ -6,7 +6,7 @@ echo -e "---------------------\n7.4-apache:\n---------------------\n\n" \
 && bash BuildAndPush.sh Dockerfile-8.0-apache \
 && echo -e "---------------------\n8.1-apache:\n---------------------\n\n" \
 && bash BuildAndPush.sh Dockerfile-8.1-apache \
-&& echo -e "---------------------\n8.1-apache-bullseye:\n---------------------\n\n" \
-&& bash BuildAndPush.sh Dockerfile-8.1-apache-bullseye \
 && echo -e "---------------------\n8.2-apache:\n---------------------\n\n" \
-&& bash BuildAndPush.sh Dockerfile-8.2-apache
+&& bash BuildAndPush.sh Dockerfile-8.2-apache \
+&& echo -e "---------------------\n8.3-apache:\n---------------------\n\n" \
+&& bash BuildAndPush.sh Dockerfile-8.3-apache
